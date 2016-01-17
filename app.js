4ae5f9043839a7f563977005c07fa2e5b76b9a7d
Emlyn Mileaf-Patel <gremleaf@gmail.com>
2016-01-17 00:04:32 -0500

    Updates and progress

--- app.js
+++ app.js
@@ -3,20 +3,29 @@ var path = require('path');
 var app = express();
 var bodyParser = require('body-parser');
 
-var items = []
-app.set("view engine", "ejs");
+
+app.set("view engine", "jade");
 
 app.set("views", path.join(__dirname, 'views'));
 
 app.use(bodyParser());
+
 app.get("/", function (req, res) {
-  //res.json({
-    //status: "ok",
-		//version: "1"
-  //});
   console.log("i has hacked all ur bases");
   res.render('index');
 })
+
+app.get("/main/", function(req, res){
+  res.render('main');
+})
+app.get("/home/", function(req,res){
+  res.render('home');
+})
+
+
+var api_groups = require('./routes/api_index');
+
+
 app.listen(1337, function(){
   console.log("server running on port 1337");
 })
